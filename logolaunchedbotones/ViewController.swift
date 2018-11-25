5ed2d1afdd7be0cba4fead85778008f0e429524e
Enric Miro <enricmirochi@gmail.com>
2018-11-25 20:54:12 +0100

    Initial Commit

---
+++ logolaunchedbotones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  logolaunchedbotones
+//
+//  Created by Arnau Miró Maestre on 25/11/18.
+//  Copyright © 2018 Enric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
